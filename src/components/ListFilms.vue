3613c08151865813f3a8210b21e423fd3cd3373a
fork_web <leshadgrad@mail.ru>
2020-09-11 18:14:32 +0400

    nav_done

--- src/components/ListFilms.vue
+++ src/components/ListFilms.vue
@@ -7,27 +7,27 @@
       <ul class="cd-accordion-menu animated">
         <li class="has-children">
           <input type="checkbox" name ="group-1" id="group-1" checked>
-          <label for="group-1">Действия</label>
+          <label class="title_menu_drop active_slide_menu" for="group-1">Действия</label>
           <ul>
-            <li><a href="#0">Просмотры</a></li>
-            <li><a href="#0">Избранное</a></li>
-            <li><a href="#0">Отложенное</a></li>
+            <li class="drop_data"><a href="#0">Просмотры</a></li>
+            <li class="drop_data drop_data_active"><a href="#0">Избранное</a></li>
+            <li class="drop_data"><a href="#0">Отложенное</a></li>
           </ul>
         </li>
 
         <li class="has-children">
           <input type="checkbox" name ="group-2" id="group-2">
-          <label for="group-2">Тарифы</label>
+          <label class="title_menu_drop" for="group-2">Тарифы</label>
 
           <ul>
-            <li><a href="#0">Максимальный</a></li>
-            <li><a href="#0">Средний</a></li>
-            <li><a href="#0">Минимальный</a></li>
+            <li class="drop_data"><a href="#0">Максимальный</a></li>
+            <li class="drop_data"><a href="#0">Средний</a></li>
+            <li class="drop_data"><a href="#0">Минимальный</a></li>
           </ul>
         </li>
 
         <li class="has-children">
-          <a href="#0">Настрйоки</a>
+          <a class="drop_data title_menu_drop" href="#0">Настрйоки</a>
         </li>
 
       </ul>
@@ -70,8 +70,10 @@ export default {
             //detect change in the input[type="checkbox"] value
             accordion.on('change', 'input[type="checkbox"]', function(){
               var checkbox = $(this);
+              var label = $(this).parent(".has-children").find(".title_menu_drop");
               console.log(checkbox.prop('checked'));
               ( checkbox.prop('checked') ) ? checkbox.siblings('ul').attr('style', 'display:none;').slideDown(300) : checkbox.siblings('ul').attr('style', 'display:block;').slideUp(300);
+              ( checkbox.prop('checked') ) ? label.addClass('active_slide_menu') : label.removeClass("active_slide_menu");
             });
           });
         }

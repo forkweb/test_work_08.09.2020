2a2c1506f16685e1409ebd018242c94ed5716e61
fork_web <leshadgrad@mail.ru>
2020-09-11 15:06:06 +0400

    item_done

--- src/components/ListFilms.vue
+++ src/components/ListFilms.vue
@@ -1,14 +1,39 @@
 <template>
 <div class="favorites">
   <div class="nav-bar">
-    Меню
-    <p>asdsa</p>
-    <p>asdsa</p>
-    <p>asdsa</p>
-    <p>asdsa</p>
-    <p>asdsa</p>
-    <p>asdsa</p>
-    <p>asdsa</p>
+    
+    <nav class="main-nav">
+      <ul class="cd-accordion-menu animated">
+        <li class="has-children">
+          <!-- <input type="checkbox" name ="group-1" id="group-1" checked> -->
+          <input type="checkbox" name ="group-1" id="group-1">
+          <label for="group-1">Действия</label>
+
+              <ul>
+                <li><a href="">Просмотры</a></li>
+                <li><a href="">Избранное</a></li>
+                <li><a href="">Отложенное</a></li>
+              </ul>
+        </li>
+
+        <li class="has-children">
+          <input type="checkbox" name ="group-2" id="group-2">
+          <label for="group-2">Тарифы</label>
+
+          <ul>
+            <li><a href="">Максимальный</a></li>
+            <li><a href="">Средний</a></li>
+            <li><a href="">Минимальный</a></li>
+          </ul>
+        </li>
+
+
+        <li class="has-children">
+          <a href="">Настройки</a>
+        </li>
+      </ul>
+    </nav>
+
   </div>
   <div class="container">
     <div class="title">Избранное</div>

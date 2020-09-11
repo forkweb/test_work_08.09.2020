1a3296c7a1a2c03f53acf8fba0bc591383596ce4
fork_web <leshadgrad@mail.ru>
2020-09-11 15:36:44 +0400

    add_menu

--- src/components/ListFilms.vue
+++ src/components/ListFilms.vue
@@ -1,19 +1,18 @@
 <template>
 <div class="favorites">
   <div class="nav-bar">
-    
+
     <nav class="main-nav">
+
       <ul class="cd-accordion-menu animated">
         <li class="has-children">
-          <!-- <input type="checkbox" name ="group-1" id="group-1" checked> -->
-          <input type="checkbox" name ="group-1" id="group-1">
+          <input type="checkbox" name ="group-1" id="group-1" checked>
           <label for="group-1">Действия</label>
-
-              <ul>
-                <li><a href="">Просмотры</a></li>
-                <li><a href="">Избранное</a></li>
-                <li><a href="">Отложенное</a></li>
-              </ul>
+          <ul>
+            <li><a href="#0">Просмотры</a></li>
+            <li><a href="#0">Избранное</a></li>
+            <li><a href="#0">Отложенное</a></li>
+          </ul>
         </li>
 
         <li class="has-children">
@@ -21,16 +20,16 @@
           <label for="group-2">Тарифы</label>
 
           <ul>
-            <li><a href="">Максимальный</a></li>
-            <li><a href="">Средний</a></li>
-            <li><a href="">Минимальный</a></li>
+            <li><a href="#0">Максимальный</a></li>
+            <li><a href="#0">Средний</a></li>
+            <li><a href="#0">Минимальный</a></li>
           </ul>
         </li>
 
-
         <li class="has-children">
-          <a href="">Настройки</a>
+          <a href="#0">Настрйоки</a>
         </li>
+
       </ul>
     </nav>
 
@@ -50,12 +49,41 @@
 
 <script>
 import ItemFilms from '../components/ItemFilms.vue'
+import $ from "jquery";
 
 export default {
   name: 'ListFilms',
   components: {
     ItemFilms
-  }
+  },
+  watch: {
+  },
+  methods: {
+    Menu() {
+      $(document).ready(function(){
+        var accordionsMenu = $('.cd-accordion-menu');
+
+        if( accordionsMenu.length > 0 ) {
+          
+          accordionsMenu.each(function(){
+            var accordion = $(this);
+            //detect change in the input[type="checkbox"] value
+            accordion.on('change', 'input[type="checkbox"]', function(){
+              var checkbox = $(this);
+              console.log(checkbox.prop('checked'));
+              ( checkbox.prop('checked') ) ? checkbox.siblings('ul').attr('style', 'display:none;').slideDown(300) : checkbox.siblings('ul').attr('style', 'display:block;').slideUp(300);
+            });
+          });
+        }
+      });
+    }
+  },
+  create(){
+    
+  },
+  mounted() {
+    this.Menu();
+  },
 }
 </script>
 
